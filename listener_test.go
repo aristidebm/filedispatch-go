e34b6c80f09738a2d37cddcccd699bd58e982c34
Aristide <aristide.bamazi@imsp-uac.org>
2024-01-06 19:17:30 +0100

    Wip: Add tests on listener

--- listener_test.go
+++ listener_test.go
@@ -1,7 +1,10 @@
 package filedispatch
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,7 +21,8 @@ func TestIgnoreDirs(t *testing.T) {
 	listener, err := NewListener(router)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("unable to instiante the listener")
+		return
 	}
 
 	listener = listener.WithIgnoreDirs("/tmp/dir1", "/tmp/dir3").
@@ -41,13 +45,90 @@ func TestIgnoreDirs(t *testing.T) {
 }
 
 func TestIgnoreFiles(t *testing.T) {
+	router := FakeRouter{}
+	listener, err := NewListener(router)
 
+	if err != nil {
+		log.Printf("unable to instiante the listener")
+		return
+	}
+
+	listener = listener.WithIgnoreFiles("/tmp/file1.txt", "/tmp/file3.txt").
+		WithIgnoreFiles("/tmp/file4.txt", "file5.txt", "dir2/file8.txt").WithIgnoreFiles("^private.txt$")
+
+	assert.True(t, listener.canHandleFile("/tmp/storage/file1.txt"))
+	assert.True(t, listener.canHandleFile("/tmp/private.txt"))
+
+	assert.False(t, listener.canHandleFile("/tmp/file1.txt"))
+	assert.False(t, listener.canHandleFile("dir2/file8.txt"))
+	assert.False(t, listener.canHandleFile("/tmp/dir2/file8.txt"))
+	assert.False(t, listener.canHandleFile("/home/dir2/file8.txt"))
+	assert.False(t, listener.canHandleFile("file5.txt"))
+	assert.False(t, listener.canHandleFile("/home/user/file5.txt"))
+	assert.False(t, listener.canHandleFile("/tmp/file5.txt"))
+	assert.False(t, listener.canHandleFile("private.txt"))
 }
 
-func TestIgnoreRecusrvice(t *testing.T) {
+func TestRecusrvice(t *testing.T) {
+	tmpDir, err := prepareTestDirTree("dir/to/walk/")
 
+	if err != nil {
+		fmt.Printf("unable to create test dir tree: %v\n", err)
+		return
+	}
+
+	defer os.RemoveAll(tmpDir)
+
+	router := FakeRouter{}
+	listener, err := NewListener(router)
+
+	if err != nil {
+		log.Printf("unable to instiante the listener")
+		return
+	}
+
+	listener = listener.WithRecursive(false)
+	paths, err := listener.getPaths(tmpDir)
+
+	if err != nil {
+		log.Printf("unable paths")
+		return
+	}
+
+	assert.Len(t, paths, 1)
+	assert.Equal(t, paths[0], tmpDir)
+
+	listener = listener.WithRecursive(true)
+	paths, err = listener.getPaths(tmpDir)
+
+	if err != nil {
+		log.Printf("unable to get the paths")
+		return
+	}
+	expected := []string{tmpDir, filepath.Join(tmpDir, "dir"), filepath.Join(tmpDir, "dir/to"), filepath.Join(tmpDir, "dir/to/walk")}
+	assert.Len(t, paths, 4)
+	assert.Equal(t, paths, expected)
 }
 
-func TestIgnoreWatch(t *testing.T) {
+func TestRecusrviceAndIgnoreDir(t *testing.T) {
+	//
+}
 
+func TestRecusrviceAndIgnoreFile(t *testing.T) {
+	//
+}
+
+// source https://github.com/golang/go/blob/master/src/path/filepath/example_unix_walk_test.go#L16C1-L29C2
+func prepareTestDirTree(tree string) (string, error) {
+	tmpDir, err := os.MkdirTemp("", "")
+	if err != nil {
+		return "", fmt.Errorf("error creating temp directory: %v\n", err)
+	}
+
+	err = os.MkdirAll(filepath.Join(tmpDir, tree), 0755)
+	if err != nil {
+		os.RemoveAll(tmpDir)
+		return "", err
+	}
+	return tmpDir, nil
 }
